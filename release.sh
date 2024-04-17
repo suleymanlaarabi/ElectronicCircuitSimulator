4c195db1f3052ea8f5faa7647f7743358200e858
suleymanlaarabi <suleyman.laarabi.dev@gmail.com>
2024-04-17 22:08:31 +0200

    release

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ mv ./target/release/circuit-sim ./target/release/circuit-sim-LINUX
 mv ./target/x86_64-pc-windows-gnu/release/circuit-sim ./target/x86_64-pc-windows-gnu/release/circuit-sim-WINDOWS
 
 # publish release
-gh release create v1.0 ./target/release/circuit-sim-LINUX ./target/x86_64-pc-windows-gnu/release/circuit-sim-WINDOWS
+gh release create v1.0 ./target/release/circuit-sim-LINUX ./target/x86_64-pc-windows-gnu/release/circuit-sim-WINDOWS.exe
 
 
 
